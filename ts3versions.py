7ebd02f3fe34c48df14da5ef131471bcfdc25cca
Andreas Heil <ah@linux-hq.de>
2015-01-05 06:00:31 +0100

    initial import

---
+++ ts3versions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import lxml.html
+import json
+from distutils.version import LooseVersion
+
+baseurl = 'http://dl.4players.de/ts/releases/'
+data = {'latest': None, 'versions': []}
+
+releases = lxml.html.parse(baseurl).getroot()
+for element,attribute,rlink,pos in releases.iterlinks():
+  if element.tag is 'a' \
+  and attribute is 'href' \
+  and rlink.startswith('3.'):
+    files = lxml.html.parse(baseurl + rlink).getroot()
+    for element,attribute,flink,pos in files.iterlinks():
+      if element.tag is 'a' \
+      and attribute is 'href' \
+      and flink.find('-server_') > 0:
+        data['versions'].append(rlink.strip('/'))
+        break
+
+data['versions'].sort(key=LooseVersion)
+data['latest'] = data['versions'][-1]
+
+print json.dumps(data, sort_keys=True, indent=2)
